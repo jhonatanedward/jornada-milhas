77320013455adf06742a4ae527f5c352cdf3a2c9
Edward Jhon <jhonataneds@gmail.com>
2024-06-24 11:13:08 -0300

    Ajustes e conclusão do modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,9 @@ import { MatChipsModule } from '@angular/material/chips';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import {MatNativeDateModule} from '@angular/material/core';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ModalComponent } from './shared/modal/modal.component';
 
 @NgModule({
   declarations: [
@@ -35,7 +37,8 @@ import {MatNativeDateModule} from '@angular/material/core';
     HomeComponent,
     CardBuscaComponent,
     CardDepoimentoComponent,
-    FormBuscaComponent
+    FormBuscaComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
@@ -50,7 +53,8 @@ import {MatNativeDateModule} from '@angular/material/core';
     MatFormFieldModule,
     MatInputModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
